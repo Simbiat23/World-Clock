387a7f7b9737b323dcb4832e96197f759a0758ad
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-03-20 21:32:58 +0000

    Added a third city

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 // london
 function updateTime() {
   let londonElement = document.querySelector("#london");
-
   let londonDateElement = londonElement.querySelector("#london .date");
-
   let londonTimeElement = londonElement.querySelector("#london .time");
   let londonTime = moment().tz("London/Europe");
 
@@ -11,14 +9,12 @@ function updateTime() {
     .tz("Europe/London")
     .format("MMMM Do YYYY");
   londonTimeElement.innerHTML = londonTime.format(
-    "H:mm:ss:SS [<small>]A[</small>]"
+    "HH:mm:ss:SS [<small>]A[</small>]"
   );
 
   // Paris
   let parisElement = document.querySelector("#paris");
-
   let parisDateElement = parisElement.querySelector("#paris .date");
-
   let parisTimeElement = parisElement.querySelector("#paris .time");
   let parisTime = moment().tz("Europe/Paris");
 
@@ -26,7 +22,18 @@ function updateTime() {
     .tz("Paris/Europe")
     .format("MMMM Do YYYY");
   parisTimeElement.innerHTML = parisTime.format(
-    "H:mm:ss:SS [<small>]A[</small>]"
+    "HH:mm:ss:SS [<small>]A[</small>]"
+  );
+
+  //   Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector("#tokyo .date");
+  let tokyoTimeElement = tokyoElement.querySelector("#tokyo .time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = moment().tz("Asia/Tokyo").format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format(
+    "HH:mm:ss:SS [<small>]A[<small>]"
   );
 }
 
